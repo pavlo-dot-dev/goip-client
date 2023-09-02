bd5edb380efcada9d96d519d84469f7faf150f6f
PP <pp@pp>
2023-09-02 16:07:51 +0300

    Save session on cookie file

--- src/Abstract/Client.php
+++ src/Abstract/Client.php
@@ -3,6 +3,8 @@
 namespace PavloDotDev\GoipClient\Abstract;
 
 use GuzzleHttp\Cookie\CookieJar;
+use GuzzleHttp\Cookie\FileCookieJar;
+use GuzzleHttp\Exception\RequestException;
 use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\DomCrawler\Crawler;
 
@@ -14,16 +16,20 @@ abstract class Client
     protected ?string $content = null;
     protected ?ResponseInterface $response = null;
 
-    public function __construct(string $baseUri, string $login, string $password)
+    public function __construct(string $baseUri, string $login, string $password, string $cookiePath = null)
     {
         $this->baseUri = $baseUri.(mb_substr($baseUri, 0, -1) !== '/' ? '/' : '');
 
-        $cookieJar = new CookieJar();
+        if ($cookiePath) {
+            $cookieJar = new FileCookieJar($cookiePath, true);
+        } else {
+            $cookieJar = new CookieJar();
+        }
 
         $this->client = new \GuzzleHttp\Client([
             'base_uri' => $this->baseUri,
             'cookies' => $cookieJar,
-            'timeout' => 30,
+            'timeout' => 30
         ]);
 
         $this->auth($login, $password);
@@ -32,6 +38,13 @@ abstract class Client
     public function auth(string $login, string $password): void
     {
         $crawler = $this->request('main.php');
+        if ($crawler->filter('form[name="login"]')->count() === 0) {
+            $this->authorized = true;
+            return;
+        }
+
+        echo 'Авторизация!';
+
         $form = $crawler->filter('form[name="login"]')->form();
         $formValues = $form->getValues();
         $formValues['username'] = $login;
